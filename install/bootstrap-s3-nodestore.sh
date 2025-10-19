610417f530badac50c6724243a440790b4cf37dd
Rijn Bian <bxbian951122@gmail.com>
2025-10-18 22:49:01 -0400

    Patch with registry mirrors

--- install/bootstrap-s3-nodestore.sh
+++ install/bootstrap-s3-nodestore.sh
@@ -1,6 +1,7 @@
 echo "${_group}Bootstrapping seaweedfs (node store)..."
 
 $dc up --wait seaweedfs postgres
+$dc exec seaweedfs sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
 $dc exec -e "HTTP_PROXY=${HTTP_PROXY:-}" -e "HTTPS_PROXY=${HTTPS_PROXY:-}" -e "NO_PROXY=${NO_PROXY:-}" -e "http_proxy=${http_proxy:-}" -e "https_proxy=${https_proxy:-}" -e "no_proxy=${no_proxy:-}" seaweedfs apk add --no-cache s3cmd
 $dc exec seaweedfs mkdir -p /data/idx/
 s3cmd="$dc exec seaweedfs s3cmd"
